b8c8eedbf1fbfcfaacb7331ad4b4c1bbdc41da6c
BRVNDXN <brandon@BrandonsworkMBP.T-mobile.com>
2017-05-29 17:17:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Define global variables
+global.rootRequire = function (name) {
+  return require(__dirname + '/' + name);
+}
+
+const express = require('express');
+const app = express();
+
+app.set('views', (__dirname + '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
